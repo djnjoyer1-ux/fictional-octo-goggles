6412f8583fb2e0836ae92dc205aee60d8cfa5e75
djnjoyer1-ux <djnjoyer1@gmail.com>
2026-08-03 22:27:00 -0700

    Add files via upload

---
+++ learnhowtoprogram/hacker1.py
@@ -0,0 +1,7 @@
+# This program outputs "Hello, World!" to the screen.
+def main():
+    # Debug: Check if the print statement is correct
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
