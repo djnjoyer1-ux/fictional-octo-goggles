672d51a45bde669c37449966930939c981e48c6b
djnjoyer1-ux <djnjoyer1@gmail.com>
2026-08-03 20:51:00 -0700

    Add files via upload

---
+++ 2025/1.py
@@ -0,0 +1,9 @@
+j=0
+i=1
+while i < 1000:
+    if i % 3 == 0 or i % 5 == 0:
+        j+=i
+    i+=1
+
+print(j)
+    
---
+++ 2025/12days.py
@@ -0,0 +1,16 @@
+import math
+
+i=1
+j=1
+k=0
+while i<=12:
+    j=i
+    #print(j)
+    while j > 0:
+        k+=j
+        j-=1
+    print(k)
+    i+=1
+
+print (k)
+    
